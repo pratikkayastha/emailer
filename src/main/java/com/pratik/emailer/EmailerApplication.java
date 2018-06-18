bc6f97f831ca6c2fc8c524cd8b93234219480189
pratikkayastha <pratik.kayastha@gmail.com>
2018-06-18 03:17:17 +0000

    Changes to server port and loading process of truststore

--- src/main/java/com/pratik/emailer/EmailerApplication.java
+++ src/main/java/com/pratik/emailer/EmailerApplication.java
@@ -3,6 +3,10 @@ package com.pratik.emailer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class EmailerApplication {
@@ -15,11 +19,16 @@ public class EmailerApplication {
 			 * Had to import Mailgun's certificate to a truststore
 			 * The truststore being used is /src/main/resources/cacerts
 			 * */
-			String path = new ClassPathResource("cacerts").getFile().getPath();
-			System.setProperty("javax.net.ssl.trustStore", path);
+			ClassPathResource classPathResource = new ClassPathResource("cacerts");
+			InputStream inputStream = classPathResource.getInputStream();
+			File tempFile = File.createTempFile("cacerts", "");
+			FileUtils.copyInputStreamToFile(inputStream, tempFile);
+
+			System.setProperty("javax.net.ssl.trustStore", tempFile.getAbsolutePath());
 			System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 		} catch (Exception e) {
 			System.out.println("Could not set truststore; HTTPS call might not work!");
+			e.printStackTrace();
 		}
 
 		SpringApplication.run(EmailerApplication.class, args);
